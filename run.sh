15d078829a7b674696204d42e53c08d781691ad4
Slava Tutrinov <slava@tutrinov.pro>
2019-12-10 21:57:53 +0300

    config to /usr/local, fix configmap

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java ${JVM_OPTS} -Dlogback.configurationFile=./logback-console.xml -jar app.jar -conf config.json
+java ${JVM_OPTS} -Dlogback.configurationFile=./logback-console.xml -jar app.jar -conf /usr/local/config.json
